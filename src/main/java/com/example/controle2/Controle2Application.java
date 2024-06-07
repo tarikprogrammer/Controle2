19ddaa66b8e61cf8afa5b94313b1ceba584154fd
tarikprogrammer <belaid.tarikk@gmail.com>
2024-06-07 10:56:34 +0100

    test

---
+++ src/main/java/com/example/controle2/Controle2Application.java
@@ -0,0 +1,13 @@
+package com.example.controle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controle2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controle2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/controle2/Controle2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controle2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controle2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
